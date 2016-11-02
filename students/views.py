96374efe6a882250317be1e2674b985a9f12903c
Vasil Matiyash <matiyash.pr@ukr.net>
2016-11-02 22:23:24 +0200

    Динамізував сторінку, статичні ресурси

--- students/views.py
+++ students/views.py
@@ -1,8 +1,28 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def students_list(request):
-	return render(request, 'students/students_list.html', {})
+	students = (
+        {'id' : 1,
+         'first_name' : u'Олег',
+         'last_name' : u'Курилас',
+         'ticket' : 2123,
+         'image' : 'img/Kurilas.jpg'},
+      
+      {'id' : 2,
+       'first_name' : u'Вячеслав',
+       'last_name' : u'Дрофа',
+       'ticket' : 1147,
+       'image' : 'img/Drofa.jpg'},
+     
+      {'id' : 3,
+       'first_name' : u'Любомир',
+       'last_name' : u'Пристанський',
+       'ticket' : 3422,
+       'image' : 'img/Pristansky.jpg'},
+    )
+        return render(request, 'students/students_list.html', {'students': students})
 
 def students_add(request):
 	return HttpResponse('<h1>Student Add Form</h1>')
